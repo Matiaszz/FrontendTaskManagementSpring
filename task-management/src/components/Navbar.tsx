75ddfa0374de58c81c11a37eb90214705a50583b
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-04-05 10:43:56 -0300

    fix: Links and isDark in navbar fixed

--- task-management/src/components/Navbar.tsx
+++ task-management/src/components/Navbar.tsx
@@ -9,24 +9,27 @@ const Navbar = () => {
 
 
     const switchAction = () => {
-        setIsDark((prev) => !prev)
+        setIsDark((prev) => {
+            const newValue = !prev;
 
-        if (!isDark) {
-            document.getElementById("body")?.classList.add("dark");
-            return;
-        }
-        document.getElementById("body")?.classList.remove("dark");
+            if (newValue) {
+                document.getElementById("body")?.classList.add("dark");
+            } else {
+                document.getElementById("body")?.classList.remove("dark");
+            }
+
+            return newValue;
+        });
     };
-
     return (
         <div className="flex w-full h-16 bg-[#2974AA] shadow-[inset_0px_35px_30px_#205F8D] border border-black justify-between items-center p-6">
 
-            <h1 className="font-mono text-2xl text-white">TaskFlow</h1>
+            <a className='select-none' href="/"><h1 className="font-mono hover:cursor-pointer  text-2xl text-white">TaskFlow</h1></a>
 
             <div className='flex gap-6'>
                 <Switch onCheckedChange={switchAction} />
-                <FolderIcon />
-                <UserIcon />
+                <a href=""><FolderIcon /></a>
+                <a href="/profile"><UserIcon /></a>
             </div>
         </div>
     )
